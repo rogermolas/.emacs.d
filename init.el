cd612ae631819cfc4664ec1f6cc5608d201d43b9
Roger Molas <r.molas06@gmail.com>
2017-11-10 16:21:33 +0800

    initial commit

---
+++ init.el
@@ -0,0 +1,56 @@
+;;; Package ---- "init.el" Created by Roger Molas on 07/11/15.
+;;; Code:
+;;; Commentary:
+;;;   Customization and basic tools used for development using Emacs
+;;;
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Commentary
+;; ========== ========== ========== ========== ========== ==========
+;; ************************ Configuration **************************
+;; ========== ========== ========== ========== ========== ==========
+;; Code:
+(add-to-list 'load-path "~/.emacs.d/config")
+(add-to-list 'load-path "~/.emacs.d/theme/")
+(require 'settings-config)
+(require 'theme-dark)
+;(require 'plugins-config)
+;(require 'mode-config)
+
+
+;; Commentary
+;; ========== ========== ========== ========== ========== ==========
+;; ****************** LOAD PACKAGES REPOSITORY *********************
+;; ========== ========== ========== ========== ========== ==========
+;; Code:
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   '("melpa" .    "http://melpa.org/packages/")
+   t)
+  (package-initialize))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(comint-scroll-to-bottom-on-input t)
+ '(fringe-mode (quote (4 . 0)) nil (fringe))
+ '(indicate-buffer-boundaries
+   (quote
+    ((t . left)
+     (top . left)
+     (bottom . left)
+     (up . left)
+     (down . left)))))
+
+(provide 'init) ;;; init.el ends here
+
