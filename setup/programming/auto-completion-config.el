631b19115cb991a3ad9b46d8ea29ec3bc2fa74d6
Roger Molas <r.molas06@gmail.com>
2017-12-21 22:23:40 +0800

    update auto complete

--- setup/programming/auto-completion-config.el
+++ setup/programming/auto-completion-config.el
@@ -6,20 +6,5 @@
 (add-to-list 'load-path "~/.emacs.d/plugin-lisp/popup")
 (add-to-list 'load-path "~/.emacs.d/plugin-lisp/auto-complete")
 (require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
 (ac-config-default)
-(setq ac-use-menu-map t)
-
-;; auto-complete hook
-(setq-default ac-sources
-	      '(ac-source-yasnippet ac-source-abbrev ac-source-dictionary ac-source-words-in-same-mode-buffers))
-(add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup)
-(add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
-(add-hook 'ruby-mode-hook 'ac-ruby-mode-setup)
-(add-hook 'css-mode-hook 'ac-css-mode-setup)
-(add-hook 'auto-complete-mode-hook 'ac-common-setup)
-(global-auto-complete-mode t)
-(add-to-list 'ac-modes 'objc-mode)
-
 (provide 'auto-completion-config) ;;; auto-completion-config.el ends here
-
