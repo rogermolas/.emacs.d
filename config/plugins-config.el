19d15305ade6e987a492673bade76771190cdbeb
Roger Molas <r.molas06@gmail.com>
2018-01-18 17:01:49 +0800

    Add iedit mode feature

--- config/plugins-config.el
+++ config/plugins-config.el
@@ -69,6 +69,9 @@
 ;; yasnippet
 (require 'yasnippet-config)
 
+;; iedit
+(require 'iedit-config)
+
 ;; ========== ========== ========== ========== ==========
 ;; **************** Commands/Environment ****************
 ;; ========== ========== ========== ========== ==========
