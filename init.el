26d9f2ea159979ebe3267d95ac7401f4ab72fc74
Roger Molas <r.molas06@gmail.com>
2018-04-09 01:06:32 +0800

    Remove theme

--- init.el
+++ init.el
@@ -4,6 +4,13 @@
 ;;;   Customization and basic tools used for development using Emacs
 ;;;
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -21,26 +28,29 @@
 (require 'settings-config)
 (require 'plugins-config)
 (require 'mode-config)
-(require 'theme-dark)
+;;(load-theme )
+;(require 'github-modern)
 
 ;; Commentary
 ;; ========== ========== ========== ========== ========== ==========
 ;; ****************** LOAD PACKAGES REPOSITORY *********************
 ;; ========== ========== ========== ========== ========== ==========
 ;; Code:
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (add-to-list
-   'package-archives
-   '("melpa" .    "http://melpa.org/packages/")
-   t)
-  (package-initialize))
+(require 'package)
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+(package-initialize)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(anzu-deactivate-region t)
+ '(anzu-mode-lighter "")
+ '(anzu-replace-threshold 50)
+ '(anzu-replace-to-string-separator " => ")
+ '(anzu-search-threshold 1000)
  '(comint-scroll-to-bottom-on-input t)
  '(fringe-mode (quote (4 . 0)) nil (fringe))
  '(indicate-buffer-boundaries
@@ -50,7 +60,9 @@
      (bottom . left)
      (up . left)
      (down . left))))
- '(package-selected-packages (quote (magit))))
+ '(package-selected-packages
+   (quote
+    (greymatters-theme github-theme grandshell-theme github-modern-theme magit))))
 
 (provide 'init) ;;; init.el ends here
 
