33138b3c82e6647d9651357b24fa99362615d666
Roger Molas <r.molas06@gmail.com>
2018-07-29 18:15:27 +0800

    Add flycheck, modify setup and config file

---
+++ setup/programming/flycheck-config.el
@@ -0,0 +1,8 @@
+;;; package ---- "flycheck-config.el" Created by Roger Molas on 07/29/18.
+;;; Commentary:
+;;;    Customization and configuration for flycheck.
+;;; Code:
+
+(setq global-flycheck-mode t)
+
+(provide 'flycheck-config) ;;; flycheck-config.el ends here
