87055245e92fff46676a5b49bea3cf8ad6cf2ca8
Roger Molas <r.molas06@gmail.com>
2018-08-19 21:28:50 +0800

    Remove unused snippets

--- config/mode-config.el
+++ config/mode-config.el
@@ -8,18 +8,6 @@
 ;; Installed / Downloaded Modes
 ;; ================================================
 
-;;Markdown Mode customization for development using Emacs
-;;https://github.com/jrblevin/markdown-mode
-(add-to-list 'load-path "~/.emacs.d/plugin-lisp-modes/markdown")
-(autoload 'markdown-mode "markdown-mode"
-   "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-
-(autoload 'gfm-mode "markdown-mode"
-   "Major mode for editing GitHub Flavored Markdown files" t)
-(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
-
 ;; C/C++
 ;; Use irony package from melpa repository
 (add-hook 'c++-mode-hook 'irony-mode)
--- setup/programming/yasnippet-config.el
+++ setup/programming/yasnippet-config.el
@@ -5,22 +5,9 @@
 
 (add-to-list 'load-path "~/.emacs.d/plugin-lisp/yasnippet")
 (require 'yasnippet)
-(add-to-list 'yas-snippet-dirs "~/.emacs.d/plugin-lisp/snippets/snippets")
+(setq yas-snippet-dirs
+      '("~/.emacs.d/plugin-lisp/yasnippet/snippets/" ;; snippets
+        ))
 (yas-global-mode 1)
-
-; let's define a function which initializes auto-complete-c-headers and gets called for c/c++ hooks
-(defun my:ac-c-header-init ()
-  ;(require 'auto-complete-c-headers)
-  ;(add-to-list 'ac-sources 'ac-source-c-headers)
-  (add-to-list 'achead:include-directories '"/usr/local/include")
-  (add-to-list 'achead:include-directories '"/Applications/Developer Tools/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1")
-  (add-to-list 'achead:include-directories '"/Applications/Developer Tools/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/9.0.0/include")
-  (add-to-list 'achead:include-directories '"/Applications/Developer Tools/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include")
-  (add-to-list 'achead:include-directories '"/Applications/Developer Tools/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.13.sdk/usr/include")
-  (add-to-list 'achead:include-directories '"/Applications/Developer Tools/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.13.sdk/System/Library/Frameworks")
- )
-; now let's call this function from c/c++ hooks
-(add-hook 'c++-mode-hook 'my:ac-c-header-init)
-(add-hook 'c-mode-hook 'my:ac-c-header-init)
 
 (provide 'yasnippet-config) ;;; yasnippet-config.el ends here
