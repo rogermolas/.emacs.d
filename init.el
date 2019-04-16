494a2918b2a7b86b907474f547042cb4a4d397ca
Roger Molas <r.molas06@gmail.com>
2019-04-16 19:04:05 +0800

    change stage repository

--- init.el
+++ init.el
@@ -21,8 +21,7 @@
 ;; ========== ========== ========== ========== ========== ==========
 ;; Code:
 (require 'package)
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("melpa-stable" . "https://stable.melpa.org/packages/")))
+(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")))
 (package-initialize)
 
 (when (not package-archive-contents)
@@ -49,3 +48,17 @@
 
 (provide 'init)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (company-quickhelp magit markdown-mode pyvenv company-anaconda anaconda-mode company yasnippet-snippets yasnippet flycheck-pycheckers flycheck-pos-tip))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
